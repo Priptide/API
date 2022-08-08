485880bfa7a1521b23123a4e1d84304683e313cd
Charles Crooke <chacrook@WKMZT564108D.global.publicisgroupe.net>
2022-08-08 10:01:29 +0100

    Added session ids and error checking for starting a conversation

--- services/lex.ts
+++ services/lex.ts
@@ -1,26 +1,27 @@
 import { RecognizeTextCommand } from "@aws-sdk/client-lex-runtime-v2";
 import lexClient from "../config/awsConfig";
 
-async function start_conversation(message: string) {
+async function start_conversation(message: string, sessionId: string) {
+  //Check we have a valid message
+  if (!message) throw new Error("No valid message");
+
+  //Check we have a session id
+  if (!sessionId) throw new Error("Missing session id");
+
   //Spin up a new client
   const client = lexClient();
 
-  //Create a command for recognition
+  //Create a command for text recognition
   const command = new RecognizeTextCommand({
     text: message,
     botId: process.env.BOT_ID ?? "",
     botAliasId: process.env.BOT_ALIAS_ID ?? "",
     localeId: process.env.LOCALE_ID ?? "",
-    sessionId: "new_session",
+    sessionId: sessionId,
   });
 
-  try {
-    const data = await client.send(command);
-    console.log(data);
-  } catch (error) {
-    // error handling.
-    console.log("Error" + error);
-  }
+  //Attempt to get the data required from the AWS lex server
+  const data = await client.send(command);
 }
 
 export default { start_conversation };
