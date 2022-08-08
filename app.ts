539b41aeafda7b83963961310bfb58fe56d62416
Siana Dicheva <siadiche@WKMZTF43BE76.global.publicisgroupe.net>
2022-08-08 14:03:54 +0100

    express server added

--- app.ts
+++ app.ts
@@ -8,3 +8,25 @@ connect();
 connection.once("open", async () => {
     console.log("Connected to database");
 });
+
+//server setup 
+
+import express from "express";
+const app = express();
+
+//port set-up
+ const port = process.env.SERVER_PORT;
+
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.send("hello");
+} );
+
+// start the express server
+app.listen( port, () => {
+    
+    console.log( `server started at http://localhost:${ port }` );
+} );
+
+
+
