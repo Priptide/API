53133a016787568534404d29169e95bcf91931d1
Charles Crooke <chacrook@WKMZT564108D.global.publicisgroupe.net>
2022-08-09 14:31:52 +0100

    Added service to records for creating or finding conditionally

--- routes/lex.ts
+++ routes/lex.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import LexService from "../services/lex";
+import RecordService from "../services/record";
 
 //Setup a router
 const lex_routes = express.Router();
@@ -8,14 +9,30 @@ const lex_routes = express.Router();
 lex_routes.post("/send", async (req, res, next) => {
     try {
         //TODO: Create a new record for this chat
+        const { id, session_id, uuid } = await RecordService.find_or_create(
+            req.body.language ?? "en_gb",
+            req.body.name,
+            req.body.uuid,
+            req.body.session_id
+        );
 
         //Get all possible intentions.
         const intentions = await LexService.send_message(
             req.body.message ?? "",
-            req.body.sessionId ?? ""
+            session_id
         );
-
-        res.status(200).json({ intentions });
+        if (intentions && intentions.length > 0) {
+            res.status(200).json({
+                uuid: uuid,
+                session_id: session_id,
+                message: intentions[0].intent?.name,
+                intentions: intentions,
+            });
+        } else {
+            res.status(500).json({
+                message: "Internal Server Error - No Results Found",
+            });
+        }
     } catch (error: any) {
         //Check if it is a known error.
         if (error.message == "Missing session id") {
