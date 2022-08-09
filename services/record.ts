9fb248e0e6aa6b6f5f82b971c14b160ca937081f
Charles Crooke <chacrook@WKMZT564108D.global.publicisgroupe.net>
2022-08-09 13:51:36 +0100

    Updated record service to include session id and is active variables

--- services/record.ts
+++ services/record.ts
@@ -1,41 +1,42 @@
 import RecordModel, { Record } from "../models/record";
 
-
 //Inserting a record
-async function insert_record(uuid: string, name: string, language: string){
-    const data:Record = {
+async function insert_record(uuid: string, name: string, language: string) {
+    const data: Record = {
         UUID: uuid,
-        chat:{
+        chat: {
             language: language,
             conversation: [],
         },
         name: name,
-    }
+        session_id: "random_id",
+        is_active: true,
+    };
     const record = await new RecordModel(data).save();
 
-    return {id: record._id};
+    return { id: record._id };
 }
 
 //Finding a record
-async function find_record(){
+async function find_record() {
     await RecordModel.find({})
-    .then(res =>{
-        console.log({res});
-    })
-    .catch(err =>{
-        console.log(err);
-    })
+        .then((res) => {
+            console.log({ res });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
 // Getting a record by UUID
-async function find_byId_record( uuid : String){
+async function find_byId_record(uuid: String) {
     await RecordModel.findById(uuid)
-    .then(res =>{
-        console.log({res});
-    })
-    .catch(err =>{
-        console.log(err);
-    })
+        .then((res) => {
+            console.log({ res });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
-export default {insert_record, find_record, find_byId_record}; 
+export default { insert_record, find_record, find_byId_record };
