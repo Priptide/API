8d933edb2852b6542f5ebeff47476c8293963673
Siana Dicheva <siadiche@WKMZTF43BE76.global.publicisgroupe.net>
2022-08-10 14:54:16 +0100

    basic testing

---
+++ routes/__tests__/endpoint.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import express from "express";
+
+const app = express();
+const baseURL = "http://127.0.0.1:3000/action"
+
+describe("GET /testdrive-click", () => {
+  it("clicks testdrive", async () => {
+    const result = await request(app).get("/testdrive-click");
+    expect(result.body.message).toContain("https://www.ford.co.uk/shop/test-drive");
+    expect(result.statusCode).toEqual(200);
+  });
+});
+
