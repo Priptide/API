2e71bd1166948abf2c185b817efde63ce0db23f1
Charles Crooke <chacrook@WKMZT564108D.global.publicisgroupe.net>
2022-08-25 15:28:53 +0100

    Updated to post request and allowed them to write into their own records

--- routes/actions.ts
+++ routes/actions.ts
@@ -1,11 +1,33 @@
+import RecordService from "../services/record";
 import express from "express";
 
 const actions_routes = express.Router();
 
-actions_routes.get("/testdrive-click", async (req, res, next) => {
+actions_routes.post("/testdrive-click", async (req, res, next) => {
     try {
+        //Create or update the current record with the users input message
+        const { session_id, uuid } = await RecordService.update_record(
+            "Book a Test Drive",
+            false,
+            req.body.uuid,
+            req.body.session_id,
+            req.body.language,
+            req.body.name
+        );
+
+        await RecordService.update_record(
+            "https://www.ford.co.uk/shop/test-drive",
+            false,
+            uuid,
+            session_id,
+            req.body.language,
+            req.body.name
+        );
+
         res.status(200).json({
             message: "https://www.ford.co.uk/shop/test-drive",
+            uuid: uuid,
+            session_id: session_id,
         });
     } catch (error: any) {
         //Check if it is a known error.
@@ -16,8 +38,27 @@ actions_routes.get("/testdrive-click", async (req, res, next) => {
     }
 });
 
-actions_routes.get("/book-service-click", async (req, res, next) => {
+actions_routes.post("/book-service-click", async (req, res, next) => {
     try {
+        //Create or update the current record with the users input message
+        const { session_id, uuid } = await RecordService.update_record(
+            "Book a Service",
+            false,
+            req.body.uuid,
+            req.body.session_id,
+            req.body.language,
+            req.body.name
+        );
+
+        //Update what the bot replies
+        await RecordService.update_record(
+            "https://www.ford.co.uk/support/book-a-service/dealer-step",
+            false,
+            uuid,
+            session_id,
+            req.body.language,
+            req.body.name
+        );
         res.status(200).json({
             message:
                 "https://www.ford.co.uk/support/book-a-service/dealer-step",
@@ -30,8 +71,26 @@ actions_routes.get("/book-service-click", async (req, res, next) => {
         next(error);
     }
 });
-actions_routes.get("/price-comp-click", async (req, res, next) => {
+actions_routes.post("/price-comp-click", async (req, res, next) => {
     try {
+        const { session_id, uuid } = await RecordService.update_record(
+            "Price Comparison",
+            false,
+            req.body.uuid,
+            req.body.session_id,
+            req.body.language,
+            req.body.name
+        );
+
+        await RecordService.update_record(
+            "http://www.compare.ford.co.uk/home?kee=380519",
+            false,
+            uuid,
+            session_id,
+            req.body.language,
+            req.body.name
+        );
+
         res.status(200).json({
             message: "http://www.compare.ford.co.uk/home?kee=380519",
         });
